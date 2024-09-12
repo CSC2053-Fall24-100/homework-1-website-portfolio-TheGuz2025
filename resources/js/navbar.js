60ba2d5f3555482b2eaf57ec2dff7fb1e2586578
TheGuz2025 <mark.guzmanl2025@gmail.com>
2024-09-12 13:52:26 -0400

    Update navbar.js

--- resources/js/navbar.js
+++ resources/js/navbar.js
@@ -1,1 +1,26 @@
-
+function loadNavbar() {
+    const navbar = `
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Mark Guzman Lizarazo</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="index.html">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="schedule.html">Schedule</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="hobbies.html">Hobbies</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    `;
+    document.getElementById('navbar-container').innerHTML = navbar;
+}
